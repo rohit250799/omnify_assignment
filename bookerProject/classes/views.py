c72120543638e5a21fce0bebb5fbf070233db69d
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-06-09 16:12:33 +0530

    add: get classes method to return all fitness classes list in JSON format

--- bookerProject/classes/views.py
+++ bookerProject/classes/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import FitnessClass
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, you reached the classes index")
+
+def get_classes(request):
+    if request.method == "GET":
+        fitnessClasses = FitnessClass.objects.all()
+        returnData = []
+
+    for fitness_class in fitnessClasses:
+        returnData.append({
+            "id": fitness_class.id,
+            "name": fitness_class.name,
+            "date_time": fitness_class.date_time,
+            "instructor": fitness_class.instructor
+        })
+
+    return JsonResponse(returnData, safe=False)
