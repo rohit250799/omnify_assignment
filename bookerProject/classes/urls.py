743a89e4ab699cc37e88b5df3d740c248a31bfe7
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-06-09 16:11:27 +0530

    add: path for getting list of all fitness classes using GET request

--- bookerProject/classes/urls.py
+++ bookerProject/classes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("classes/", views.get_classes, name="get_classes"),
 ]
