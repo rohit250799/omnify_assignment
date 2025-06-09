22b2d9e8ed204793f9f879b737a05e848d0434d1
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-06-09 12:33:24 +0530

    add: User and Fitness class models, based on assumptions

--- bookerProject/classes/models.py
+++ bookerProject/classes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User:
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f'{self.name} - {self.email}'
+
+class FitnessClass:
+    name = models.CharField(max_length=255)
+    date_time = models.DateTimeField()
+    instructor = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.name} - {self.date_time}'
+    
