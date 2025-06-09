2595779c281a56a30d4c03f880722fd4cb998482
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-06-09 13:21:31 +0530

    add: classes app in installed apps list and related migration files

--- bookerProject/classes/models.py
+++ bookerProject/classes/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class User:
+class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
@@ -11,7 +11,7 @@ class User:
     def __str__(self):
         return f'{self.name} - {self.email}'
 
-class FitnessClass:
+class FitnessClass(models.Model):
     name = models.CharField(max_length=255)
     date_time = models.DateTimeField()
     instructor = models.CharField(max_length=255)
@@ -19,3 +19,20 @@ class FitnessClass:
     def __str__(self):
         return f'{self.name} - {self.date_time}'
     
+class AvailableSlot(models.Model):
+    fitness_class = models.ForeignKey(FitnessClass, related_name='slots', on_delete=models.CASCADE)
+    slot_time = models.DateTimeField()
+    capacity = models.PositiveIntegerField(default=1) #represents how many users can book this 
+
+    def __str__(self):
+        return f'{self.fitness_class.name} - {self.slot_time}'
+
+#The Booking model acts as a through model between User and FitnessClass, while also connecting with AvailableSlot to show which slots were booked    
+class Bookings(models.Model):
+    user = models.ForeignKey(User, related_name='bookings', on_delete=models.CASCADE)
+    fitness_class = models.ForeignKey(FitnessClass, related_name='bookings', on_delete=models.CASCADE)
+    slots_booked = models.ManyToManyField(AvailableSlot)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.user.name} booked {self.quantity} slots in {self.fitness_class.name}'
