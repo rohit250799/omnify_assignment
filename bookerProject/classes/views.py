ab3e908a805cfbb82d1bf4378b314c54341f7064
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-06-09 16:43:18 +0530

    add: function to book fitness class

--- bookerProject/classes/views.py
+++ bookerProject/classes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from .models import FitnessClass
+from .models import FitnessClass, Bookings
+
+import json
 # Create your views here.
 
 def index(request):
@@ -20,3 +22,36 @@ def get_classes(request):
         })
 
     return JsonResponse(returnData, safe=False)
+
+def book_class(request):
+    if request.method != "POST": return JsonResponse({'error': 'Only POST request is allowed'}, status=405)
+
+    body = json.loads(request.body)
+    fitnessClass_id = body.get('id')
+    bookerName = body.get('name')
+    bookerEmail = body.get('email')
+
+    if not all([fitnessClass_id, bookerName, bookerEmail]):
+        return JsonResponse({'error': 'Required fields missing'}, status=400)
+    
+    fitness_class = FitnessClass.objects.all(id=fitnessClass_id)
+    availableSlot = None
+
+    for slot in fitness_class.slots.all():
+        if slot.available_count() > 0:
+            availableSlot = slot
+            break
+
+    if not availableSlot: return JsonResponse({'error': 'Slot not available in the program'}, status=400)
+
+    #creating the booking
+    Bookings.objects.create(
+        user_name=bookerName,
+        user_email=bookerEmail,
+        fitness_class=fitness_class,
+        slot=availableSlot
+    )
+
+    return JsonResponse({'success': f'Booking confirmed for {bookerName} in {fitness_class.name} at {availableSlot.slot_time}'})
+
+
