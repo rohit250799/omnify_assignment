508dd6a7075e96d48830384a7643cd76c6863cb8
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-06-11 07:54:05 +0530

    add: fitness class and available slot serializer

--- bookerProject/classes/serializers.py
+++ bookerProject/classes/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
-from .models import Bookings
+from .models import Bookings, FitnessClass, AvailableSlot
 
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bookings
         fields = ['id', 'user', 'bookedWithMail', 'fitness_class', 'slots_booked', 'quantity', 'createdAt']
         read_only_fields = ['bookedWithMail', 'createdAt']
+
+class FitnessClassSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FitnessClass
+        fields = ['id', 'name', 'date_time', 'instructor']
+
+class AvailableSlotSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AvailableSlot
+        fields = ['id', 'slot_time', 'capacity']
